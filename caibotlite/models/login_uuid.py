666188484506d82aa64a9a58c3ce86757c3109c3
ACaiCat <13110818005@qq.com>
2025-07-30 20:30:59 +0800

    fix: 修复无法登录同一设备

--- caibotlite/models/login_uuid.py
+++ caibotlite/models/login_uuid.py
@@ -15,7 +15,7 @@ class LoginUUID(Base):
     __tablename__ = "login_uuid"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
-    uuid: Mapped[str] = mapped_column(String(32), unique=True)
+    uuid: Mapped[str] = mapped_column(String(32))
     record_time: Mapped[datetime] = mapped_column()
     user_open_id: Mapped[str] = mapped_column(ForeignKey("user.open_id"))
     user: Mapped["User"] = relationship(back_populates="uuids", lazy='joined')
